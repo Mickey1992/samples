128e643d975f72c10b8269a689b9b5dca60468b8
孙国强 <master@pygmalion.click>
2015-07-20 18:18:47 +0900

    optimized version

--- images.js
+++ images.js
@@ -1,9 +1,21 @@
+var frameWidth;
+var imagesCount = 105;
+var speed = 100;
+var template = "images/1 ({INDEX}).jpg";
+function slideImages(container) {
+	container.style.left = (-frameWidth) + "px";
+	container.innerHTML = generateImageHTML(imagesCount) + generateImageHTML(1) + generateImageHTML(2);
+}
+function generateImageHTML(index) {
+	return '<img src="' + template.replace("{INDEX}", index) + '" id = "' + index + '" />';
+}
 document.addEventListener("DOMContentLoaded", function() {
+	frameWidth = parseInt(window.getComputedStyle(document.querySelector("#frame"), null).getPropertyValue("width"));
 	document.querySelector("#left-arrow").addEventListener("click", function() {
-		moveDiv(document.querySelector("#container"), -800, 100,leftMoveChange);
+		moveDiv(document.querySelector("#container"), -frameWidth, speed,leftMoveChange);
 	});
 	document.querySelector("#right-arrow").addEventListener("click", function() {
-		moveDiv(document.querySelector("#container"), 800, 100,rightMoveChange);
+		moveDiv(document.querySelector("#container"), frameWidth, speed,rightMoveChange);
 	});
 });
 function moveDiv(div, distance, time, callback){
@@ -22,22 +34,40 @@ function moveDiv(div, distance, time, callback){
 function leftMoveChange(div){
 	var firstImg = div.querySelector("img");
 	var midIndex = parseInt(firstImg.nextSibling.id);
-	var addIndex = (midIndex === 105)?2:(midIndex === 104)?1:midIndex+2;						
+	var addIndex;
+	if (midIndex == imagesCount - 1) {
+		addIndex = 1;
+	}
+	else if (midIndex === imagesCount) {
+		addIndex = 2;
+	}
+	else {
+		addIndex = midIndex + 2;
+	}
 	div.removeChild(firstImg);
 	div.innerHTML = div.innerHTML.trim() + '<img src="images/1 ('+addIndex+').jpg" id="' + addIndex + '" />';
-	div.style.left = parseInt(div.style.left) + 800+"px";			
+	div.style.left = parseInt(div.style.left) + frameWidth+"px";			
 }
 
 function rightMoveChange(div){
 	var firstImg = div.querySelector("img");
 	var midIndex = parseInt(firstImg.nextSibling.id);
-	var addIndex = (midIndex ===1)?104:((midIndex === 2)?105:midIndex-2);			
+	var addIndex;
+	if (midIndex == 1) {
+		addIndex = imagesCount - 1;
+	}
+	else if (midIndex === 2) {
+		addIndex = imagesCount;
+	}
+	else {
+		addIndex = midIndex-2;
+	}
 	var img = document.createElement("img");
 	img.src = "images/1 ("+addIndex+").jpg";
 	img.id = addIndex;
 	div.insertBefore(img,firstImg);
 	div.removeChild(div.lastElementChild);	
-	div.style.left = parseInt(div.style.left) - 800+"px";
+	div.style.left = parseInt(div.style.left) - frameWidth+"px";
 }
 
 // container.removeChild(img);
